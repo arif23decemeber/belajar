330bff4c7096f831e50bfdefc5d90b4b6d2b753e
Muhamad Arif <syamarifmuhamad@gmail.com>
2024-07-29 21:27:18 +0700

    layout

--- part6.php
+++ part6.php
@@ -49,6 +49,31 @@
         </div>
     </nav>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-6 p-2">
+                <div class="bg-dark" style="height: 200px;">
+
+                </div>
+            </div>
+            <div class="col-md-3 col-6 p-2">
+                <div class="bg-dark" style="height: 200px;">
+
+                </div>
+            </div>
+            <div class="col-md-3 col-6 p-2">
+                <div class="bg-dark" style="height: 200px;">
+
+                </div>
+            </div>
+            <div class="col-md-3 col-6 p-2">
+                <div class="bg-dark" style="height: 200px;">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
